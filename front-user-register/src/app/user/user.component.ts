d03e6a74f7bf780eb6ef6a51f5bc7e84c04711bf
JuanDavidG31 <jdgonzalezh@unbosque.edu.co>
2025-05-03 20:44:24 -0500

    Inicio de sesion y crear cuenta
    Se realizo el front de inicio de sesion y crear cuenta

--- front-user-register/src/app/user/user.component.ts
+++ front-user-register/src/app/user/user.component.ts
@@ -7,5 +7,46 @@ import { Component } from '@angular/core';
   styleUrl: './user.component.scss'
 })
 export class UserComponent {
+  user = '';
+  password = '';
 
+  name = '';
+  cedula = '';
+  username = '';
+  tPassword = '';
+  country = '';
+  address = '';
+
+  constructor() {
+  }
+
+  iniciarSesion() {
+    console.log('Usuario:', this.user);
+    console.log('Contraseña:', this.password);
+    this.resetInpusLogin();
+  }
+
+  crearCuenta() {
+    console.log('Nombre Completo:', this.name);
+    console.log('Cédula:', this.cedula);
+    console.log('Usuario:', this.username);
+    console.log('Contraseña:', this.tPassword);
+    console.log('País:', this.country);
+    console.log('Dirección:', this.address);
+    this.resetInpusCreate();
+  }
+
+  resetInpusLogin() {
+    this.user = '';
+    this.password = '';
+  }
+
+  resetInpusCreate() {
+    this.name = '';
+    this.cedula = '';
+    this.username = '';
+    this.tPassword = '';
+    this.country = '';
+    this.address = '';
+  }
 }
